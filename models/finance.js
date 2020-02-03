cda5700945c1812c168f6286c318ee0a791ab991
Kalogon <ghlee5084@kaist.ac.kr>
2020-02-03 23:38:50 +0900

    add:finance-feather

--- models/finance.js
+++ models/finance.js
@@ -30,6 +30,28 @@ var financeSchema = mongoose.Schema({
 
 financeSchema.methods.stackFinance = function (finance){
     this.market_price.push(finance["market_price"])
+    this.trade_volume.push(finance["trade_volume"])
+    this.market_cap_rank.push(finance["market_cap_rank"])
+    this.listed_stocks.push(finance["listed_stocks"])
+    this.face_value.push(finance["face_value"])
+    this.trading_unit.push(finance["trading_unit"])
+    this.foreigner_stock_limit.push(finance["foreigner_stock_limit"])
+    this.foreigner_stock_possession.push(finance["foreigner_stock_possession"])
+    this.foreigner_exhaustion_rate.push(finance["foreigner_exhaustion_rate"])
+    this.investing_opinion.push(finance["investing_opinion"])
+    this.target_price.push(finance["target_price"])
+    this.best.push(finance["best"])
+    this.worst.push(finance["worst"])
+    this.per_fn.push(finance["per_fn"])
+    this.eps_fn.push(finance["eps_fn"])
+    this.per_krx.push(finance["per_krx"])
+    this.eps_krx.push(finance["eps_krx"])
+    this.per_expect.push(finance["per_expect"])
+    this.eps_expect.push(finance["eps_expect"])
+    this.pbr.push(finance["pbr"])
+    this.bps.push(finance["bps"])
+    this.dividend_yield.push(finance["dividend_yield"])
+    this.same_industry_per.push(finance["same_industry_per"])
     return this.save();
 }
 
